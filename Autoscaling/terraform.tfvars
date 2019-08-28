a382c0299fa77e9e81f28e56254b426ea9b16fab
vineet30376 <30829577+vineet30376@users.noreply.github.com>
2019-08-28 14:58:43 +0530

    first commit

--- Autoscaling/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-region = "us-east-2"
-aws_access_key ="xxxxxxxxxxxxx"
-aws_secret_key = "xxxxxxxxxxxxxxxxx"
--- Autoscaling/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "region" {
-  description = "The AWS region"
-}
-
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
